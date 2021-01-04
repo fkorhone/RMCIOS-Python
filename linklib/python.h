1177753cfcfef5738f4f8ad8481cc1add705b204
Frans Korhonen <frans.korhonen@rmcios.fi>
2021-01-04 22:05:15 +0200

    Minimal python linklib: Add some python objects

--- linklib/python.h
+++ linklib/python.h
@@ -3,6 +3,11 @@
 
 #define PyAPI_FUNC(RTYPE) __declspec(dllexport) RTYPE
 
+#define Py_single_input 256
+#define Py_file_input 257
+#define Py_eval_input 258
+#define Py_func_type_input 345
+
 typedef enum { 
   PyGILState_LOCKED, 
   PyGILState_UNLOCKED
@@ -13,5 +18,7 @@ PyAPI_FUNC(void) PyEval_InitThreads(void);
 PyAPI_FUNC(PyGILState_STATE) PyGILState_Ensure(void);
 PyAPI_FUNC(void) PyGILState_Release(PyGILState_STATE);
 PyAPI_FUNC(int) PyRun_SimpleString(const char *s);
+
+typedef struct _object PyObject;
 
 #endif 
